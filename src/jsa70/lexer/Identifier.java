cdd69c38bfa8c882c34030e42c7a31e70b6427c3
zaheelee <zaheelee@gmail.com>
2017-09-27 12:20:32 -0400

    Finished Identifier

--- src/jsa70/lexer/Identifier.java
+++ src/jsa70/lexer/Identifier.java
@@ -2,4 +2,25 @@ package jsa70.lexer;
 
 public final class Identifier
 {
+    private final String ID;
+
+    private Identifier(String id)
+    {
+        ID = id;
+    }
+
+    public static final Identifier build(LocationalToken token)
+            throws ParserException
+    {
+        ParserException.verify(Token.Type.ID, token);
+        return new Identifier(token.getData().get());
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Identifier{" +
+                "ID='" + ID + '\'' +
+                '}';
+    }
 }
